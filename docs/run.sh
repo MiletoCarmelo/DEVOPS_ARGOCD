9c7ccf9a51cc7083ce8c4c2e74a66734ea6c8685
miletocarmelo <carmelo.mileto@gmail.com>
2024-10-21 23:47:10 +0200

    u

--- docs/run.sh
+++ docs/run.sh
@@ -7,5 +7,24 @@ argocd app create argocd-components \
 
 # => application 'argocd-components' created
 
-helm template -i -f ./argocd-components/Chart.yaml argocd-components ./argocd-components/
+helm template -f ./argocd-components/Chart.yaml argocd-components ./argocd-components/
 helm upgrade -i -f ./argocd-components/Chart.yaml argocd-components ./argocd-components/
+
+
+argocd app create trading-strategy-analysis-dev \
+  --repo https://github.com/MiletoCarmelo/DEVOPS_DEPLOY_TradingStrategyAnaylsis.git \
+  --path umbrella-trading-strategy-analysis \
+  --dest-server https://kubernetes.default.svc \
+  --dest-namespace dev \
+  --sync-policy automated \
+  --project quant-cm \
+  --values values.dev.yaml
+
+argocd app create trading-strategy-analysis-prod \
+  --repo https://github.com/MiletoCarmelo/DEVOPS_DEPLOY_TradingStrategyAnaylsis.git \
+  --path umbrella-trading-strategy-analysis \
+  --dest-server https://kubernetes.default.svc \
+  --dest-namespace prod \
+  --sync-policy automated \
+  --project quant-cm \
+  --values values.prod.yaml
