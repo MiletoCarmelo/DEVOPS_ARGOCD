4fbb6774300997253bcd5735348e27f471ec3196
miletocarmelo <carmelo.mileto@gmail.com>
2024-10-21 20:55:16 +0200

    u

--- docs/run.sh
+++ docs/run.sh
@@ -1,6 +1,11 @@
 argocd app create argocd-components \
-  --repo https://github.com/your-username/DEVOPS_ARGOCD.git \
+  --repo https://github.com/MiletoCarmelo/DEVOPS_ARGOCD.git \
   --path argocd-components \
   --dest-server https://kubernetes.default.svc \
   --dest-namespace argocd \
   --sync-policy automated
+
+# => application 'argocd-components' created
+
+helm template -i -f ./argocd-components/Chart.yaml argocd-components ./argocd-components/
+helm upgrade -i -f ./argocd-components/Chart.yaml argocd-components ./argocd-components/
